a470030a51ac074a44368d2c4faf2f6ccc9d525a
vchawla3 <vchawla3@ncsu.edu>
2018-04-09 13:58:47 -0400

    made base classes

---
+++ Client.java
@@ -0,0 +1,9 @@
+public class Client {
+	int port;
+	String host;
+	
+	public Client(int port, String host) {
+		this.port = port;
+		this.host = host;
+	}
+}
---
+++ RFC.java
@@ -0,0 +1,11 @@
+public class RFC {
+	int type;
+	String title;
+	String host;
+
+	public RFC(int type, String title, String host){
+		this.type = type;
+		this.title = title;
+		this.host = host;
+	}
+}
